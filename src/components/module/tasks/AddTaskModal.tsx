054b490ff18f95b6d050b8eaa50bcd6a79d72097
Shakil000 <racer.shakil111@gmail.com>
2025-06-30 23:47:53 +0600

    update

--- src/components/module/tasks/AddTaskModal.tsx
+++ src/components/module/tasks/AddTaskModal.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
   Dialog,
   DialogClose,
@@ -17,16 +18,26 @@ import {
   FormLabel,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 
 export function AddTaskModal() {
+  const form = useForm();
 
-    const form = useForm();
-
-    const onSubmit = (data) => {
-        console.log(data);
-    }
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <Dialog>
       <form>
@@ -34,45 +45,129 @@ export function AddTaskModal() {
           <Button variant="outline">Add Task</Button>
         </DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
-            <DialogDescription>Please Fill Upp the form</DialogDescription>
+          <DialogDescription>Please Fill Upp the form</DialogDescription>
           <DialogHeader>
             <DialogTitle>Add Task</DialogTitle>
           </DialogHeader>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}>
-                {/* Field for Title */}
-                <FormField
-              control={form.control}
-              name="title"
-              render={({field}) => (
-                <FormItem>
-                  <FormLabel className="text-fuchsia-700">Task Title</FormLabel>
-                  <FormControl> 
-                    <Input {...field} value={field.value || ""}/> 
+              {/* Field for Title */}
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-fuchsia-700">
+                      Task Title
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} value={field.value || ""} />
                     </FormControl>
-                </FormItem>
-              )}
-            />
-                {/* Field for Description */}
-                <FormField
-              control={form.control}
-              name="description"
-              render={({field}) => (
-                <FormItem>
-                  <FormLabel />
-                  <FormLabel className="text-fuchsia-700">Task Description</FormLabel>
-                  <FormControl> 
-                    <Textarea {...field} value={field.value || ""}/> 
+                  </FormItem>
+                )}
+              />
+              {/* Field for Description */}
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel />
+                    <FormLabel className="text-fuchsia-700">
+                      Task Description
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea {...field} value={field.value || ""} />
                     </FormControl>
-                </FormItem>
-              )}
-            />
-            <DialogFooter>
-              <DialogClose asChild>
-                <Button variant="outline">Cancel</Button>
-              </DialogClose>
-              <Button type="submit">Save changes</Button>
-            </DialogFooter>
+                  </FormItem>
+                )}
+              />
+              {/* Field for Due Date */}
+               <FormField
+          control={form.control}
+          name="date"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Select Date</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-full pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    // disabled={(date) =>
+                    //   date > new Date() || date < new Date("1900-01-01")
+                    // }
+                    captionLayout="dropdown"
+                  />
+                </PopoverContent>
+              </Popover>
+            </FormItem>
+          )}
+        />
+              {/* Field for Is completed */}
+              <FormField
+                control={form.control}
+                name="Priority"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-fuchsia-700">Priority</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl className="w-full">
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select your priority to display" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="Low">Low</SelectItem>
+                        <SelectItem value="Medium">Medium</SelectItem>
+                        <SelectItem value="High">High</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              {/* Field for Description */}
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel />
+                    <FormLabel className="text-fuchsia-700">Priority</FormLabel>
+                    <FormControl>
+                      <Textarea {...field} value={field.value || ""} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DialogClose>
+                <Button type="submit">Save changes</Button>
+              </DialogFooter>
             </form>
           </Form>
         </DialogContent>
